c6671a99f4537c3988d743bffb389533dd771828
Shivprakashpathak <129204194+Shivprakashpathak@users.noreply.github.com>
2026-07-16 21:55:03 +0530

    Add files via upload

---
+++ handling.java
@@ -0,0 +1,11 @@
+public class handling {
+    public static void main(String[] args) {
+    System.out.println("Shiv Prakash Pathak  - 22SCSE1040189");
+    int[] my_arr = { 1,2,3,4,5,6};
+    try {
+    System.out.println(my_arr[8]);
+    } catch (Exception e) {
+    System.out.println("Array Out of Bound!");
+    }
+    }
+   }
