0d7b9fc666240de16cebc0152cb59bd87cf35dc8
Shivprakashpathak <129204194+Shivprakashpathak@users.noreply.github.com>
2026-07-20 23:33:56 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class even {
+    public static void main (String [] args){
+        System.out.println("Shiv prakash pathak \n22scse1040189");
+        Scanner s = new Scanner (System.in);
+        System.out.print("Enter the number");
+        int n = s.nextInt();
+        if (n % 2 == 0)
+        System.out.println(n + " even");
+        else
+        System.out.println(n + " odd");
+    }  
+    }
+    
+
